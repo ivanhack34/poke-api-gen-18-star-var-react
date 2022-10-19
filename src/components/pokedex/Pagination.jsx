b2ccf8aa307daa9ffe92731ad3f631c895b9bc4e
Titodelux <79545098+Titodelux@users.noreply.github.com>
2022-10-19 17:19:29 -0400

    Dev (#1)
    * first & last
    
    * first & last
    
    * first & last

--- src/components/pokedex/Pagination.jsx
+++ src/components/pokedex/Pagination.jsx
@@ -2,7 +2,7 @@ import { current } from '@reduxjs/toolkit'
 import React from 'react'
 import './styles/pagination.css'
 
-const Pagination = ({page, pagesLength, setPage}) => {
+const Pagination = ({ page, pagesLength, setPage }) => {
 
     const pagesPerBlock = 8
     const currentBlock = Math.ceil(page / pagesPerBlock)
@@ -11,7 +11,7 @@ const Pagination = ({page, pagesLength, setPage}) => {
     const arrePages = []
     const initialPage = (currentBlock - 1) * pagesPerBlock + 1
     const limitPage = /*initialPage + pagesPerBlock - 1*/ blockLength === currentBlock ? pagesLength : currentBlock * pagesPerBlock
-    for(let i = initialPage; i<= limitPage; i++){
+    for (let i = initialPage; i <= limitPage; i++) {
         arrePages.push(i)
     }
 
@@ -26,32 +26,43 @@ const Pagination = ({page, pagesLength, setPage}) => {
     const handlePage = currentPage => {
         setPage(currentPage)
     }
+    const handleFirst = () => {
+        setPage(1)
+    }
+    const handleLast = () => {
+        setPage(pagesLength)
+    }
 
-  return (
-    <div className='pagination'>
+    return (
+        <div className='pagination'>
 
-        {
-            page > 1 &&
-            <div onClick={handlePrev} className='pagination__prev pagination__active'>&#60;</div>
-        }
-        <div>...</div> 
-        <ul className='pagination__container'>
             {
-                arrePages.map(e => (
-                    <li className={`pagination__page ${page === e && 'pagination__active'}`}
-                    onClick={() => handlePage(e)}
-                    key={e}>{e}</li>
-                ))
+                page > 1 &&
+                <div onClick={handlePrev} className='pagination__prev pagination__active'>&#60;</div>
             }
-        </ul>
-        <div>...</div>
-        {
-            page < pagesLength &&
-            <div onClick={handleNext} className='pagination_next pagination__active'>&#62;</div>
-            
-        }
-    </div>
-  )
+            {
+                page > 1 &&
+                <div onClick={handleFirst} className='pagination__page pagination__active'>...</div>
+            }
+            <ul className='pagination__container'>
+                {
+                    arrePages.map(e => (
+                        <li className={`pagination__page ${page === e && 'pagination__active'}`}
+                            onClick={() => handlePage(e)}
+                            key={e}>{e}</li>
+                    ))
+                }
+            </ul>
+            {
+                page < pagesLength &&
+                <div onClick={handleLast} className='pagination__page pagination__active'>...</div>
+            }
+            {
+                page < pagesLength &&
+                <div onClick={handleNext} className='pagination_next pagination__active'>&#62;</div>
+            }
+        </div>
+    )
 }
 
 export default Pagination
